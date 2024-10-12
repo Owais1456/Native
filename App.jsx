07d22ecfd9f482e2a5efbc3785ffdc096108e5dd
owais1456 <oraza6982@gmail.com>
2024-10-12 21:28:52 +0500

    add files

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from './Sreen/Home'
+
+const App = () => {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default App
